4d51ba25549e2f4af1e1a51aadab3b09b28f405f
Seung hyun <0867sh@naver.com>
2023-04-03 16:28:52 +0900

    김해인

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 const express = require('express')
 const app = express()
 const port = 5000
+const bodyParser = require('body-parser')
+const {User} = require("./models/User")
+
+app.use(bodyParser.urlencoded({extended: true}))
+
+
+app.use(bodyParser.json()); 
 
 const mongoose = require('mongoose')
 
@@ -11,6 +18,37 @@ mongoose.connect('mongodb+srv://LSH:12345@cluster0.95uyika.mongodb.net/?retryWri
 app.get('/', (req, res) => {
     res.send('Hello World!')
 })
+
+// app.post('/register',async (req,res) => {
+//     //회원 가입 할때 필요한 정보들을 client에서 가져오면
+//     //그것들을 데이터 베이스에 넣어준다.
+    
+//     const user = new User(req.body)
+
+//     const result = user.save((err, userInfo)=>{
+//         if(err) return res.json({success: false, err})
+//         return res.status(200).json({
+//             success: true
+//         })
+//     })
+// })
+app.post('/register', async (req, res) => {
+    //회원가입시 필요 정보를 client에서 가져오면
+    //데이터베이스에 삽입한다
+
+    //body parser를 통해 body에 담긴 정보를 가져온다
+    const user = new User(req.body)
+
+    //mongoDB 메서드, user모델에 저장
+    const result = await user.save().then(()=>{
+        res.status(200).json({
+        success: true
+    })
+    }).catch((err)=>{
+        res.json({ success: false, err })
+    })
+})
+
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
